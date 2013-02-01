addc616efd436f8255a808b6559e5db402fca0fe
Bodrul Alam <bodrul2112@gmail.com>
2013-02-01 00:07:44 +0000

    Gray Scaling Effect
    I should make this configurable, so that it doesnt annoy people

--- scripts/drawing/Line.js
+++ scripts/drawing/Line.js
@@ -4,12 +4,14 @@ define(["thirdparty/jquery"], function( jQuery ) {
 	var Line = function() {
 		
 		this.m_pPoints = [];
-		this.m_sColor = "black";
+		//this.m_sColor = "black";
+		this.m_nGrayStart = 100;
+		this.m_nGray = this.m_nGrayStart;
+		this.m_sColor = "rgba("+this.m_nGray +","+ this.m_nGray +","+ this.m_nGray + ",1)";
 		
 		this.m_p1;
 		this.m_p2;
 	}
-	
 	
 	Line.prototype.addPoint = function( oPoint ) {
 		
@@ -26,11 +28,16 @@ define(["thirdparty/jquery"], function( jQuery ) {
 	// yup its going to some funny things 
 	Line.prototype.doFunnyThings = function() {
 		
-		
+		this.m_nGray+=2;
+		this.m_sColor = "rgba("+this.m_nGray +","+ this.m_nGray +","+ this.m_nGray + ",1)";
+		if(this.m_nGray>240){
+			this.m_nGray=240;
+		}
 	} 
 	
 	Line.prototype.render = function( ctx ) {
 		
+		this.doFunnyThings();
 		ctx.strokeStyle = this.m_sColor;
 
 		if( this.m_pPoints.length > 2 )
@@ -50,9 +57,12 @@ define(["thirdparty/jquery"], function( jQuery ) {
 		{
 			var oFirstPoint = this.m_pPoints[0];
 			
+			this.m_nGray = this.m_nGrayStart;
 			
 			for(var i=1; i<this.m_pPoints.length; i++)
 			{
+				this.doFunnyThings();
+				ctx.strokeStyle = this.m_sColor;
 				var oPoint = this.m_pPoints[i];
 				ctx.beginPath();
 				ctx.moveTo( oFirstPoint.x, oFirstPoint.y );
